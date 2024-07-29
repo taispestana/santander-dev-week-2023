53987c796789863d3c521e518986c6057ad192c4
Taís Pestana <taispestana4@gmail.com>
2024-07-29 03:26:30 +0100

    Bugfix CORS

--- src/main/java/taispestana/dio/santander_dev_week_2023/Application.java
+++ src/main/java/taispestana/dio/santander_dev_week_2023/Application.java
@@ -1,8 +1,11 @@
 package taispestana.dio.santander_dev_week_2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/",description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
